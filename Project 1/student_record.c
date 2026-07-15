aec49432c2771be677ecc25b1d04a67035d55022
Azaucifer <47237500+Azaucifer@users.noreply.github.com>
2026-07-15 11:28:48 +0530

    Created struct for students and added switch to run user input

--- Project 1/student_record.c
+++ Project 1/student_record.c
@@ -3,6 +3,14 @@
 /* This program can add, view, delete, update and search students by ID. 
    Stores them in an array of structs. Save to a .txt file   */
 
+// creating a Student struct
+typedef struct
+{
+    int id;
+    char name[50];
+    float grade;
+}Student;
+
 int main()
 {
 
@@ -18,6 +26,43 @@ int main()
         "6. Save & Exit\n"
         "Enter your choice: ");
     scanf("%d", &choice);
+
+    // creating an array for students
+    Student students[100] = {0};
+    // counter for students
+    int student_count = 0;
+
+    // using switch to run the user choice
+    switch(choice)
+    {
+        case 1:
+            printf("You chose add student\n");
+            break;
+
+        case 2:
+            printf("You chose to view all students\n");
+            break;
+        
+        case 3:
+            printf("You chose to delete student\n");
+            break;
+
+        case 4:
+            printf("You chose to update student\n");
+            break;
+
+        case 5:
+            printf("You chose search student\n");
+            break;
+
+        case 6:
+            printf("You chose to save and exit\n");
+            break;
+
+        default:
+            printf("Please type 1 - 6 only\n");
+            break;
+    }
 
     return 0;
 }
