a9ea25456db2cd233e15959af06d7ab1f25d2446
Azaucifer <47237500+Azaucifer@users.noreply.github.com>
2026-07-15 12:59:49 +0530

    Added Add Student feature

--- Project 1/student_record.c
+++ Project 1/student_record.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /* This program can add, view, delete, update and search students by ID. 
    Stores them in an array of structs. Save to a .txt file   */
@@ -13,19 +14,7 @@ typedef struct
 
 int main()
 {
-
-    // print the display text
-    // ask for user input
     int choice = 0;
-    printf("---STUDENT RECORD SYSTEM---\n"
-        "1. Add Student\n"
-        "2. View All Students\n"
-        "3. Delete Student\n"
-        "4. Update Student\n"
-        "5. Search Student\n"
-        "6. Save & Exit\n"
-        "Enter your choice: ");
-    scanf("%d", &choice);
 
     // creating an array for students
     Student students[100] = {0};
@@ -33,36 +22,66 @@ int main()
     int student_count = 0;
 
     // using switch to run the user choice
-    switch(choice)
+    do
     {
-        case 1:
-            printf("You chose add student\n");
-            break;
+        // print the display text
+        // ask for user input
+        printf("---STUDENT RECORD SYSTEM---\n"
+        "1. Add Student\n"
+        "2. View All Students\n"
+        "3. Delete Student\n"
+        "4. Update Student\n"
+        "5. Search Student\n"
+        "6. Save & Exit\n"
+        "Enter your choice: ");
+        scanf("%d", &choice);
 
-        case 2:
-            printf("You chose to view all students\n");
-            break;
+        switch(choice)
+        {
+            case 1:
+                // asking user for input
+                printf("\nPlease Add Student Details\n");
+                printf("Student ID: ");
+                scanf("%d", &students[student_count].id);
+                getchar();
+
+                printf("Enter the name: ");
+                fgets(students[student_count].name, sizeof(students[student_count].name), stdin);
+                students[student_count].name[strcspn(students[student_count].name, "\n")] = '\0';
+
+                printf("Enter your grade percent: ");
+                scanf("%f", &students[student_count].grade);
+                student_count++;
+                printf("Student added successfully\n");
+                break;
+
+            case 2:
+                printf("\nYou chose to view all students\n");
+                break;
         
-        case 3:
-            printf("You chose to delete student\n");
-            break;
+            case 3:
+                printf("\nYou chose to delete student\n");
+                break;
 
-        case 4:
-            printf("You chose to update student\n");
-            break;
+            case 4:
+                printf("\nYou chose to update student\n");
+                break;
 
-        case 5:
-            printf("You chose search student\n");
-            break;
+            case 5:
+                printf("\nYou chose search student\n");
+                break;
 
-        case 6:
-            printf("You chose to save and exit\n");
-            break;
+            case 6:
+                printf("\nYou chose to save and exit\n");
+                break;
 
-        default:
-            printf("Please type 1 - 6 only\n");
-            break;
-    }
+            default:
+                printf("\nPlease type 1 - 6 only\n");
+                break;
+        }
+    }while(choice < 1 || choice > 6);
+
+    printf("%d %s %.2f", students[0].id, students[0].name, students[0].grade);
 
     return 0;
 }
