bf165a43d8736315992df46040fa900021796e13
Azaucifer <47237500+Azaucifer@users.noreply.github.com>
2026-07-20 16:45:39 +0530

    Started building the first dashboard of the bank

--- Project 2/atm_system.c
+++ Project 2/atm_system.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// defining struct for accounts
 typedef struct
 {
     int acNum;
@@ -10,5 +11,26 @@ typedef struct
 
 int main()
 {
+    // initialising bank menu choice to 0
+    int bchoice = 0;
+
+    // initialising user menu choice to 0
+    int uchoice = 0;
+
+    // creating an array for bank accounts
+    Account account[] = {0};
+
+    // printing dashboard menu atleast once for user input
+    do
+    {
+        // printing dashboard menu
+        printf("\n====== BANK ATM SYSTEM ======\n");
+        printf("1. Create Account\n"
+           "2. Login\n"
+           "3. Save & Exit\n"
+           "\nEnter your choice: ");
+        scanf("%d", &bchoice);
+    } while(bchoice != 3);
+
     return 0;
 }
