7c1c8c63cf7b2c45f626c57b5ba6f6d10da40c16
Azaucifer <47237500+Azaucifer@users.noreply.github.com>
2026-07-29 11:01:03 +0530

    Added a comment stating what the program does

--- Project 4 - Dynamic To-Do List/todo.c
+++ Project 4 - Dynamic To-Do List/todo.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*This program is a command line to-do list,
+  this doesn't have a fixed size array, users
+  can set tasks to pending or completed & 
+  delete all the completed tasks */
+
 int main()
 {
     return 0;
