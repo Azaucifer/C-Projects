c34ec77e125e7983c71ade0d531e094ed804984c
Azaucifer <47237500+Azaucifer@users.noreply.github.com>
2026-07-29 11:42:09 +0530

    Added a function for menu choices entered by user

--- Project 4 - Dynamic To-Do List/todo.c
+++ Project 4 - Dynamic To-Do List/todo.c
@@ -5,12 +5,16 @@
   can set tasks to pending or completed & 
   delete all the completed tasks */
 
+  // defining a struct for tasks
 typedef struct
 {
     int id;
     char *description;
     int status;
 } Tasks;
+
+// function prototypes
+void menuResponse(int menuChoice);
 
 int main()
 {
@@ -22,7 +26,7 @@ int main()
             save to a file
             load from a file */
     
-    // add task
+    // main menu
     int menuChoice = 0;
 
     do
@@ -34,10 +38,52 @@ int main()
                "4. Delete Task\n"
                "5. Delete all Completed Tasks\n"
                "6. Save & Exit\n"
-               "Enter your choice: ");
+               "Enter your choice (Ex: 1 to 6): ");
         scanf("%d", &menuChoice);
+
+        menuResponse(menuChoice);
     }while(menuChoice != 6);
 
     
     return 0;
+}
+
+
+
+
+/* PROGRAM FUNCTIONS ARE DEFINED HERE */
+
+// 1. function to respond to user's menu choice
+void menuResponse(int menuChoice)
+{
+    switch(menuChoice)
+    {
+        case 1:
+            printf("\nAdd Task selected\n");
+            break;
+        
+        case 2:
+            printf("\nView All Tasks selected\n");
+            break;
+
+        case 3:
+            printf("\nMark Task as Completed selected\n");
+            break;
+        
+        case 4:
+            printf("\nDelete Task selected\n");
+            break;
+
+        case 5:
+            printf("\nDelete all Completed Tasks selected\n");
+            break;
+        
+        case 6:
+            printf("\nSave & Exit selected\n");
+            break;
+
+        default:
+            printf("\nPlease enter 1 to 6 only\n");
+            break;
+    }
 }
