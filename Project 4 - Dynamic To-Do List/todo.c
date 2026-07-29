9b8747553d4f20e88bc652cc1a3fb230a464656e
Azaucifer <47237500+Azaucifer@users.noreply.github.com>
2026-07-29 11:24:02 +0530

    Added To-do list main menu

--- Project 4 - Dynamic To-Do List/todo.c
+++ Project 4 - Dynamic To-Do List/todo.c
@@ -5,7 +5,39 @@
   can set tasks to pending or completed & 
   delete all the completed tasks */
 
+typedef struct
+{
+    int id;
+    char *description;
+    int status;
+} Tasks;
+
 int main()
 {
+    /*TODO: add task
+            view all tasks
+            mark task as complete
+            delete task
+            delete all completed tasks
+            save to a file
+            load from a file */
+    
+    // add task
+    int menuChoice = 0;
+
+    do
+    {
+        printf("\n=== TO-DO LIST MENU ===\n");
+        printf("1. Add Task\n"
+               "2. View All Tasks\n"
+               "3. Mark Task as Completed\n"
+               "4. Delete Task\n"
+               "5. Delete all Completed Tasks\n"
+               "6. Save & Exit\n"
+               "Enter your choice: ");
+        scanf("%d", &menuChoice);
+    }while(menuChoice != 6);
+
+    
     return 0;
 }
